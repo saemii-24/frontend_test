56a8ef423b91a842a1bf3e1c96771c7d16005e20
saemii-24 <hims2419@gmail.com>
2024-08-23 23:58:12 +0900

    Comment: Toggle Test 주석

--- src/components/Toggle.spec.tsx
+++ src/components/Toggle.spec.tsx
@@ -6,7 +6,9 @@ describe("Toggle Component", () => {
     render(<Toggle />);
 
     // 초기 상태가 off인지 확인
+    //role="switch" 속성을 가진 요소를 선택 aria 역할을 기준으로 찾을 수 있음.
     const toggleElement = screen.getByRole("switch");
+    // aria-checked={isOn} 가 false인지 확인
     expect(toggleElement).toHaveAttribute("aria-checked", "false");
     expect(toggleElement).toHaveClass("bg-gray-300");
     expect(toggleElement.firstChild).toHaveClass("translate-x-1");
@@ -17,6 +19,7 @@ describe("Toggle Component", () => {
 
     // 버튼 클릭
     const toggleElement = screen.getByRole("switch");
+    //fireEvent를 통해 사용자들의 다양한 동작(예: 클릭, 입력, 포커스 등)을 테스트
     fireEvent.click(toggleElement);
 
     // 클릭 후 상태가 on으로 변경되었는지 확인
