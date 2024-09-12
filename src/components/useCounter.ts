65a6c6c5e73c1d50d680377095d106c0abb99770
saemii-24 <hims2419@gmail.com>
2024-09-12 23:50:06 +0900

    feat: counter hook

---
+++ src/components/useCounter.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const useCounter = (initialValue: number = 0) => {
+  const [count, setCount] = useState(initialValue);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return { count, increment, decrement };
+};
